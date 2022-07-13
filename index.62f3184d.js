61aa1c2f8fdfefc5d3930ff90b72b288cc1cbf2d
YanaZavodovskaya <104728312+YanaZavodovskaya@users.noreply.github.com>
2022-07-13 12:10:22 +0000

    Deploying to gh-pages from @ YanaZavodovskaya/gp-uralsk@8d2d9f62281ab89413f613c12ad7fc5d484b4a73 🚀

---
+++ index.62f3184d.js
@@ -0,0 +1,2 @@
+$(window).load((function(){var e=$("gallery__container");e.imagesLoaded((function(){e.masonry({columnWidth:".gallery__item",itemSelector:".gallery__tem"})}))}));
+//# sourceMappingURL=index.62f3184d.js.map
