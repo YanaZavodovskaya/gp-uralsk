81d44ad2de3ada0a1858431c2346c30ffe16c666
YanaZavodovskaya <104728312+YanaZavodovskaya@users.noreply.github.com>
2022-07-18 01:00:26 +0600

    desctop

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -12,3 +12,18 @@
     refs.menu.classList.toggle('is-open');
   }
 })();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[sidebar-menu-open]'),
+    closeMenuBtn: document.querySelector('[sidebar-menu-close]'),
+    menu: document.querySelector('[sidebar-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle('is-open');
+  }
+})();
